49a82495c292c9a74384954795aef4db3f7c7056
Roberto Huertas <roberto.huertas@outlook.com>
2023-01-26 21:45:19 +0100

    working

--- src/lib.rs
+++ src/lib.rs
@@ -63,16 +63,16 @@ async fn list_tests(State(db): State<PgPool>) -> Result<Json<Vec<Test>>, AppErro
     Ok(Json(tests))
 }
 
-#[shuttle_service::main]
-async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_service::ShuttleAxum {
-    pool.execute(include_str!("../db/schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
-    let router = router(pool).await;
-    let sync_wrapper = SyncWrapper::new(router);
-    tracing::info!("Starting axum server");
-    Ok(sync_wrapper)
-}
+// #[shuttle_service::main]
+// async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_service::ShuttleAxum {
+//     pool.execute(include_str!("../db/schema.sql"))
+//         .await
+//         .map_err(CustomError::new)?;
+//     let router = router(pool).await;
+//     let sync_wrapper = SyncWrapper::new(router);
+//     tracing::info!("Starting axum server");
+//     Ok(sync_wrapper)
+// }
 
 async fn router(pool: PgPool) -> Router {
     Router::new()
@@ -80,4 +80,105 @@ async fn router(pool: PgPool) -> Router {
         .route("/txt", get(list_tests).post(create_test))
         .route("/txt/:id", delete(delete_test))
         .with_state(pool)
+}
+
+// going without macros
+async fn main(pool: PgPool) -> shuttle_service::ShuttleAxum {
+    pool.execute(include_str!("../db/schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+    let router = router(pool).await;
+    let sync_wrapper = SyncWrapper::new(router);
+    tracing::debug!("Starting axum server");
+    Ok(sync_wrapper)
+}
+
+async fn __shuttle_wrapper(
+    factory: &mut dyn shuttle_service::Factory,
+    runtime: &shuttle_service::Runtime,
+    logger: shuttle_service::Logger,
+) -> Result<Box<dyn shuttle_service::Service>, shuttle_service::Error> {
+    use shuttle_service::tracing_subscriber::prelude::*;
+    use shuttle_service::ResourceBuilder;
+    runtime
+        .spawn_blocking(move || {
+            let filter_layer =
+                shuttle_service::tracing_subscriber::EnvFilter::try_from_default_env()
+                    .or_else(|_| shuttle_service::tracing_subscriber::EnvFilter::try_new("DEBUG"))
+                    .unwrap();
+            shuttle_service::tracing_subscriber::registry()
+                .with(filter_layer)
+                .with(logger)
+                .init();
+        })
+        .await
+        .map_err(|e| {
+            if e.is_panic() {
+                let mes = e
+                    .into_panic()
+                    .downcast_ref::<&str>()
+                    .map(|x| x.to_string())
+                    .unwrap_or_else(|| "panicked setting logger".to_string());
+                shuttle_service::Error::BuildPanic(mes)
+            } else {
+                shuttle_service::Error::Custom(
+                    shuttle_service::error::CustomError::new(e).context("failed to set logger"),
+                )
+            }
+        })?;
+
+    let pool = shuttle_shared_db::Postgres::new()
+        .build(factory, runtime)
+        .await?;
+    runtime
+        .spawn(async {
+            main(pool)
+                .await
+                .map(|ok| Box::new(ok) as Box<dyn shuttle_service::Service>)
+        })
+        .await
+        .map_err(|e| {
+            if e.is_panic() {
+                let mes = e
+                    .into_panic()
+                    .downcast_ref::<&str>()
+                    .map(|x| x.to_string())
+                    .unwrap_or_else(|| "panicked calling main".to_string());
+                shuttle_service::Error::BuildPanic(mes)
+            } else {
+                shuttle_service::Error::Custom(
+                    shuttle_service::error::CustomError::new(e).context("failed to call main"),
+                )
+            }
+        })?
+}
+
+fn __binder(
+    service: Box<dyn shuttle_service::Service>,
+    addr: std::net::SocketAddr,
+    runtime: &shuttle_service::Runtime,
+) -> shuttle_service::ServeHandle {
+    use shuttle_service::Context;
+    runtime.spawn(async move {
+        service
+            .bind(addr)
+            .await
+            .context("failed to bind service")
+            .map_err(Into::into)
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn _create_service() -> *mut shuttle_service::Bootstrapper {
+    println!("ENTRY POINT");
+    let builder: shuttle_service::StateBuilder<Box<dyn shuttle_service::Service>> =
+        |factory, runtime, logger| Box::pin(__shuttle_wrapper(factory, runtime, logger));
+
+    let bootstrapper = shuttle_service::Bootstrapper::new(
+        builder,
+        __binder,
+        shuttle_service::Runtime::new().unwrap(),
+    );
+    let boxed = Box::new(bootstrapper);
+    Box::into_raw(boxed)
 }
